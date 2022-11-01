78a5643ae91b133f60fbae4b9fbc35aeb1bacb92
Kevin <kevinpereaenriquez@gmail.com>
2022-11-01 18:14:11 +0100

    footercv

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Footer() {
     return <>
-    <footer className="py-8 md:px-8 md:py-8 text-white bg-[#212121] hover:bg-[#424242] hover:ease-in duration-500 transform-none inline-block w-full">
+    <footer className="py-5 md:px-8 md:py-4 text-white bg-[#212121] hover:bg-[#424242] hover:ease-in duration-500 transform-none inline-block w-full">
     <div className="sm:flex sm:items-center sm:justify-between">
         <Link href='./'><h1 className='ml-10 text-4xl tracking-tighter hover:tracking-wide hiddenlogo' id='logo'>KEV</h1></Link>
         <a href="#" className="hover:underline hidden sm:block" rel="noreferrer"><p className="block text-sm px-3">© 2022 KEV Portofolio</p></a>
@@ -16,7 +16,7 @@ export default function Footer() {
                 <a href="https://www.linkedin.com/in/kevin-perea-enriquez/" className="mr-4 md:mr-6" target='_blank' rel="noreferrer"><AiFillLinkedin size={30}/></a>
             </li>
             <li className='hover:scale-75 ease-in duration-200 transform-none'>
-                <a href="https://drive.google.com/file/d/1RX53MPaM7KKXMyJSv3ML7AdSK0zMttZu/view?usp=sharing" className="mr-4 hover:underline md:mr-6 " target='_blank' rel="noreferrer"><AiFillFile size={30}/></a>
+                <a href="https://drive.google.com/file/d/1VyB_28H4yR7V_m9imxYZAQ01L1vts5aj/view?usp=share_link" className="mr-4 hover:underline md:mr-6 " target='_blank' rel="noreferrer"><AiFillFile size={30}/></a>
             </li>
             <li className='hover:scale-75 ease-in duration-200 transform-none'>
                 <a href="https://github.com/grandaragornd" className="hover:underline" target='_blank' rel="noreferrer"><AiFillGithub size={30}/></a>
